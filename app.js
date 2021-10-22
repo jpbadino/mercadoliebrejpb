a5c9a26e0e5139c398e21854d1872b924b038812
Kaorisati <kaorisati@gmail.com>
2021-10-22 13:21:35 -0300

    primer version

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app=express();
+const path=require('path');
+
+
+app.listen(3030, ()=> {console.log("Servidor corriendo en puerto 3030")})
+app.use(express.static('public')); //permite vincular o completar la ruta para que luego html busque los recursos publicos
+app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,'/views/index.html'))})
+//otras formas de determinar la ruta:
+//app.get("/",(req,res)=>{req.sendFile(__dirname+"/views/index.html")})
+//app.get("/",(req,res)=>{req.sendFile(path.join(__dirname,"/views/index.gtml"))})
+
+
