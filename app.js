554cb6f7b20da0aab96a36c230907bd4f96f8ba4
Kaorisati <kaorisati@gmail.com>
2021-10-28 08:31:59 -0300

    no me sale link con fuentes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app=express();
 const path=require('path');
 
-
 app.listen(3030, ()=> {console.log("Servidor corriendo en puerto 3030")})
 app.use(express.static('public')); //permite vincular o completar la ruta para que luego html busque los recursos publicos
 app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,'/views/index.html'))})
