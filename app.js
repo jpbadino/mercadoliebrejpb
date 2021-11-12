25196ae32e431c3662a8f3d932fc50aac9ee6338
Kaorisati <kaorisati@gmail.com>
2021-11-11 21:44:04 -0300

    version con login y algun transition

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,'/views/index.html'))})
 //otras formas de determinar la ruta:
 //app.get("/",(req,res)=>{req.sendFile(__dirname+"/views/index.html")})
 //app.get("/",(req,res)=>{req.sendFile(path.join(__dirname,"/views/index.gtml"))})
-
-
+app.get("/login",(req,res)=>{res.sendFile(path.join(__dirname,'/views/login.html'))})
+app.get("/onboarding",(req,res)=>{res.sendFile(path.join(__dirname,'/views/onboarding.html'))})
